229270ce489dc6c27ce322830357cb74266d75cd
Emil <e.yusupovone@yandex.ru>
2018-08-14 01:01:41 +0400

    itemslist

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import 'bootswatch/dist/sandstone/bootstrap.css';
 
 import ScrollDown from './Components/ScrollDown/scrolldown.jsx';
 import Navbarapp from './Components/Containers/navbar/navbar.jsx';
-import Item from './Components/Containers/items/items.jsx';
-import ItemsList from './Components/Containers/items/itemslist.jsx';
+//import Item from './Components/Containers/items/items.jsx';
+//import ItemsList from './Components/Containers/items/itemslist.jsx';
 import Createform from './Components/Forms/Button/button.jsx';
 import Welcome from './Components/Containers/welcome/welcome.jsx';
 import Main from './Components/Containers/main/main.jsx';
