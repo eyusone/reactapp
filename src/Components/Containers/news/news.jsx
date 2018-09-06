31a1ffc7cd72bd33195ecdb03edb3a9772a4fba4
Emil <e.yusupovone@yandex.ru>
2018-09-07 01:01:40 +0300

    another change by pages

--- src/Components/Containers/news/news.jsx
+++ src/Components/Containers/news/news.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import { NavLink, Switch, Route } from 'react-router-dom';
 import 'bootswatch/dist/sandstone/bootstrap.css';
+import './news.css'
 
 class News extends React.Component {
     componentDidMount() {
@@ -16,7 +17,12 @@ class News extends React.Component {
         const id = this.props.match.params.id;
         //const img = this.props.text;
         const names = ['Данное приложение','APP.COM','Обо мне'];
-        const imgs = [];
+        const imgs = ['/public/img/screen.png'];
+        const describe = [
+          's',
+          'ss',
+          'sss'
+        ];
         const urls = ['http://dxxx.netlify.com', 'http://dyyy.netlify.com', 'https://kazan.hh.ru/resume/c72d1cb8ff05b128480039ed1f72566f6f344c']
 
       let section = (
@@ -26,13 +32,21 @@ class News extends React.Component {
               <div className = "col-12 col-md-12 col-xl-12 col-sm-12 bg-light" style = {{padding: '3%', boxShadow: '10px 8px 20px -3px'}}>
 
                 {names.map((item, index)=>{if (id == index+1) {
-                  return (<h1>{item}</h1>)}})}
+                  return (<h1 className='display-4'>{item}</h1>)
+                }})}
 
-                <div className='row'>
-                  <div className='col-12'>
+                <p>
+                  {describe.map((item, index)=>{if (id == index+1) {
+                    return item
+                  }})}
+                </p>
 
-                  </div>
-                </div>
+                <p>
+                  <figure>
+                    <img src = {imgs[0]} style = {{width: '800px', height: '400px'}}/>
+                  </figure>
+                </p>
+
                 {urls.map ((item,index)=> {if (id == index+1) {
                   return (<p>Ссылка: <a href = {item}> {item}</a></p>)
                 }})}
